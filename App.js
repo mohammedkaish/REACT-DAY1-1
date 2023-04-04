12f94abe563a30aa989f31002917bb09d3c2fb5f
mohammedkaish <108514125+mohammedkaish@users.noreply.github.com>
2023-04-04 21:34:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>let see the output</h1>
+         <button onClick={window['alertmsg']}>click to see</button>
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
